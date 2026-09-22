fd20df37cac04b430d87056e2668ecee5155a818
AriasLQ <l.arias05@ufromail.cl>
2026-09-22 20:38:49 -0300

    [Dev - Luis]: implementar modelo Usuario con roles definidos

--- src/main/java/Modelo/Usuario.java
+++ src/main/java/Modelo/Usuario.java
@@ -1,4 +1,29 @@
 package Modelo;
 
 public class Usuario {
+    private final String idUsuario;
+    private String nombre;
+    private final RolUsuario rol;
+
+    public Usuario(String idUsuario, String nombre, RolUsuario rol) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.rol = rol;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public RolUsuario getRol() {
+        return rol;
+    }
 }
