34b21999f876216ac2b7c2b4d2ab3262f4e942bc
AriasLQ <l.arias05@ufromail.cl>
2026-09-24 10:36:26 -0300

    [Dev - Luis]: crear estructura base de AdminController

--- src/main/java/Controlador/AdminController.java
+++ src/main/java/Controlador/AdminController.java
@@ -1,4 +1,20 @@
 package Controlador;
 
+import Modelo.Casilla;
+import Modelo.RegistroHistorial;
+import java.util.List;
+
+
 public class AdminController {
+    private final List<Casilla> inventarioCasillas;
+    private final List<RegistroHistorial> auditoria;
+
+    public AdminController(List<Casilla> inventarioCasillas, List<RegistroHistorial> auditoria) {
+        this.inventarioCasillas = inventarioCasillas;
+        this.auditoria = auditoria;
+    }
+
+    private void registrarAccion(String idUsuario, String accion, String detalle) {
+        auditoria.add(new RegistroHistorial(idUsuario, accion, detalle));
+    }
 }
