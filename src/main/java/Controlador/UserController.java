2e8ba2924ea85118856fab80d60d2fbb1f610e46
AriasLQ <l.arias05@ufromail.cl>
2026-09-24 10:18:08 -0300

    [Dev - Luis]: implementar metodo guardarItem en UserController

--- src/main/java/Controlador/UserController.java
+++ src/main/java/Controlador/UserController.java
@@ -1,6 +1,7 @@
 package Controlador;
 
 import Modelo.Casilla;
+import Modelo.Item;
 import Modelo.RegistroHistorial;
 import java.util.List;
 
@@ -11,6 +12,17 @@ public class UserController {
     public UserController(List<Casilla> inventarioCasillas, List<RegistroHistorial> auditoria) {
         this.inventarioCasillas = inventarioCasillas;
         this.auditoria = auditoria;
+    }
+
+    public boolean guardarItem(String idCasilla, Item item, String idUsuario) {
+        for (Casilla casilla : inventarioCasillas) {
+            if (casilla.getIdCasilla().equals(idCasilla) && idUsuario.equals(casilla.getIdUsuarioAsignado())) {
+                casilla.agregarItem(item);
+                registrarAccion(idUsuario, "INGRESO_ITEM", "Agregó " + item.getNombre() + " en " + idCasilla);
+                return true;
+            }
+        }
+        return false;
     }
 
     private void registrarAccion(String idUsuario, String accion, String detalle) {
