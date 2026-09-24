e77df1b355f813403e191f02bb9d1729c086b7e2
AriasLQ <l.arias05@ufromail.cl>
2026-09-24 10:12:00 -0300

    [Dev - Luis]: definir estructura inicial y constructor de UserController

--- src/main/java/Controlador/UserController.java
+++ src/main/java/Controlador/UserController.java
@@ -1,4 +1,19 @@
 package Controlador;
 
+import Modelo.Casilla;
+import Modelo.RegistroHistorial;
+import java.util.List;
+
 public class UserController {
+    private final List<Casilla> inventarioCasillas;
+    private final List<RegistroHistorial> auditoria;
+
+    public UserController(List<Casilla> inventarioCasillas, List<RegistroHistorial> auditoria) {
+        this.inventarioCasillas = inventarioCasillas;
+        this.auditoria = auditoria;
+    }
+
+    private void registrarAccion(String idUsuario, String accion, String detalle) {
+        auditoria.add(new RegistroHistorial(idUsuario, accion, detalle));
+    }
 }
