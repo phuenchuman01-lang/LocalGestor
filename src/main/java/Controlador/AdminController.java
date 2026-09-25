bcdf311717c1b9355ee29bd412c6d2e0f23e3b67
AriasLQ <l.arias05@ufromail.cl>
2026-09-24 22:15:49 -0300

    [Dev - Luis]: añadir funcion para instanciar modulos de casillas

--- src/main/java/Controlador/AdminController.java
+++ src/main/java/Controlador/AdminController.java
@@ -14,6 +14,14 @@ public class AdminController {
         this.auditoria = auditoria;
     }
 
+    public void agregarModuloCasillas(int cantidad, String prefijo, String idAdmin) {
+        for (int i = 1; i <= cantidad; i++) {
+            String idCasilla = prefijo + "-" + i;
+            inventarioCasillas.add(new Casilla(idCasilla));
+        }
+        registrarAccion(idAdmin, "CREACION_MODULO", "Se agregaron " + cantidad + " casillas (" + prefijo + ")");
+    }
+
     private void registrarAccion(String idUsuario, String accion, String detalle) {
         auditoria.add(new RegistroHistorial(idUsuario, accion, detalle));
     }
