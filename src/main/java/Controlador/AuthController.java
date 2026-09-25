78f74be8248dbd22d6d90069fc6c76ae604f6b50
AriasLQ <l.arias05@ufromail.cl>
2026-09-25 00:08:46 -0300

    [Dev - Luis]: definir estructura inicial y constructor de AuthController

--- src/main/java/Controlador/AuthController.java
+++ src/main/java/Controlador/AuthController.java
@@ -1,4 +1,17 @@
 package Controlador;
 
+import Modelo.Usuario;
+import Modelo.RolUsuario;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AuthController {
+    private final Map<String, Usuario> usuarios;
+    private Usuario usuarioActual;
+
+    public AuthController() {
+        this.usuarios = new HashMap<>();
+        // El administrador se crea por defecto y no se puede eliminar
+        this.usuarios.put("admin", new Usuario("admin", "Administrador Principal", RolUsuario.ADMIN));
+    }
 }
